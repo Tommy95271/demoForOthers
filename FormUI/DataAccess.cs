c3b44e2709e3b6cf976df3e5979edae5fdedd6d7
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-01-19 10:36:07 +0800

    加入專案檔案。

---
+++ FormUI/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FormUI
+{
+    public class DataAccess
+    {
+        public List<Person> GetPeople(string lastName)
+        {
+            using (IDbConnection connection = new SqlConnection(Helper.CnnVal("Sample")))
+            {
+                //var output = connection.Query<Person>($"select * from People where LastName = '{ lastName }'").ToList();
+                var output = connection.Query<Person>("dbo.sp_People_GetByLastName @LastName", new { LastName = lastName }).ToList();
+                return output;
+            }
+        }
+
+        public void InsertPerson(string firstName, string lastName, string emailAddress, string phoneNumber)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("Sample")))
+            {
+                //Person newPerson = new Person { FirstName = firstName, LastName = lastName, EmailAddress = emailAddress, PhoneNumber = phoneNumber };
+                List<Person> people = new List<Person>();
+
+                people.Add(new Person { FirstName = firstName, LastName = lastName, EmailAddress = emailAddress, PhoneNumber = phoneNumber });
+
+                connection.Execute("dbo.sp_InsertPeople @FirstName, @LastName, @EmailAddress, @PhoneNumber", people);
+
+            }
+        }
+    }
+}
